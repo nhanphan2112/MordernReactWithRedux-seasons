6e7cc222d3d70fffd2b3dd85787cf2ed8e139054
Nhan Phan <nphanacc@gmail.com>
2021-05-28 10:47:20 -0700

    57-Updating State Properties using setState

--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ import ReactDOM from "react-dom";
 class App extends React.Component {
   constructor(props){
     super(props);
+    // THIS IS THE ONLY TIME we do direct assignment 
+    // to this.state
     this.state = { lat: null};
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setstate!!!!
+        this.setState({lat: position.coords.latitude})
+        // we did not!!!
+        // this.state.lat = position.coords.latitude
+      },
+      (err) => console.log(err)
+    );
+
   }
 
 
   //React says we have to define render!!
 	render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-		return <div>Lattitude: </div>;
+		return <div>Lattitude: {this.state.lat} </div>;
 	}
 }
 
