175b413a3300097e537fff4b5ca60dc60b322f31
Nhan Phan <nphanacc@gmail.com>
2021-05-31 11:24:01 -0700

    64-Alternate State Initialization

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		// THIS IS THE ONLY TIME we do direct assignment
-		// to this.state
-		this.state = { lat: null, errorMessage: "" };
-	}
+	//Alternate method to initialize state object without using contructor method with super(props)
+	state = { lat: null, errorMessage: "" };
 
 	componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
