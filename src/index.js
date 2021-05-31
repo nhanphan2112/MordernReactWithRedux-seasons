f57cbb8e93c98435008c2fd1e4d57a233f30d053
Nhan Phan <nphanacc@gmail.com>
2021-05-31 11:12:16 -0700

    63-Refractoring Data Loading to Lifecycle Modthod componentDidMount

--- src/index.js
+++ src/index.js
@@ -7,22 +7,13 @@ class App extends React.Component {
 		// THIS IS THE ONLY TIME we do direct assignment
 		// to this.state
 		this.state = { lat: null, errorMessage: "" };
-
-		window.navigator.geolocation.getCurrentPosition(
-			(position) => {
-				// we called setstate!!!!
-				this.setState({ lat: position.coords.latitude });
-				// we did not!!!
-				// this.state.lat = position.coords.latitude
-			},
-			(err) => {
-				this.setState({ errorMessage: err.message });
-			}
-		);
 	}
 
 	componentDidMount() {
-		console.log("My component was rendered to the screen");
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => this.setState({ lat: position.coords.latitude }),
+			(err) => this.setState({ errorMessage: err.message })
+		);
 	}
 
 	componentDidUpdate() {
