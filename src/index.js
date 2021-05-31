85c7f73c364cd715d573cd537c21cdf14f32544a
Nhan Phan <nphanacc@gmail.com>
2021-05-31 10:36:23 -0700

    61-Introducing Lifecycle Methods

--- src/index.js
+++ src/index.js
@@ -2,38 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    // THIS IS THE ONLY TIME we do direct assignment 
-    // to this.state
-    this.state = { lat: null, errorMessage: ''};
+	constructor(props) {
+		super(props);
+		// THIS IS THE ONLY TIME we do direct assignment
+		// to this.state
+		this.state = { lat: null, errorMessage: "" };
 
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setstate!!!!
-        this.setState({lat: position.coords.latitude})
-        // we did not!!!
-        // this.state.lat = position.coords.latitude
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message});
-      }
-    );
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => {
+				// we called setstate!!!!
+				this.setState({ lat: position.coords.latitude });
+				// we did not!!!
+				// this.state.lat = position.coords.latitude
+			},
+			(err) => {
+				this.setState({ errorMessage: err.message });
+			}
+		);
+	}
 
-  }
+	componentDidMount() {
+		console.log("My component was rendered to the screen");
+	}
 
+	componentDidUpdate() {
+		console.log("My component was just updated - it rerendered");
+	}
 
-  //React says we have to define render!!
+	//React says we have to define render!!
 	render() {
-    if(this.state.errorMessage && !this.state.lat){
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: {this.state.errorMessage}</div>;
+		}
 
-    if(!this.state.errorMessage && this.state.lat){
-      return <div>Latitude: {this.state.lat}</div>;
-    }
+		if (!this.state.errorMessage && this.state.lat) {
+			return <div>Latitude: {this.state.lat}</div>;
+		}
 
-    return <div>Loading!</div>;
+		return <div>Loading!</div>;
 	}
 }
 
